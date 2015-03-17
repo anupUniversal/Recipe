e8e9d7872d1e8769789ee4676ddb63aa44ed4990
anup1s <asharma@securview.com>
2015-03-17 11:06:25 +0530

    Change-Id: I122e45f2161553c40cfbc2b640c6d83c5390bf5a Loop added

--- src/main/java/com/securview/hotel/App.java
+++ src/main/java/com/securview/hotel/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for (int i = 0; i < 11; i++) {
+			System.out.println("he " + i);
+		}
     }
 }
